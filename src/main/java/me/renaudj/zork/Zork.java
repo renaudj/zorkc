b96f166bca60d06b68af9ea4cf37fa55a7dd86ca
Anonymous <williamsc2@windsor.k12.mo.us>
2015-11-05 19:30:30 +0000

    Test

--- src/main/java/me/renaudj/zork/Zork.java
+++ src/main/java/me/renaudj/zork/Zork.java
@@ -1,85 +1,31 @@
 package me.renaudj.zork;
 
-import java.io.IOException;
-import java.io.PrintWriter;
-import java.net.Socket;
-import java.net.UnknownHostException;
 import java.util.Scanner;
-import java.util.concurrent.atomic.AtomicBoolean;
 
-public class Zork {
+public class Zork{
+  
+  public boolean running = false;
+  
   public Zork(){
-    connect();
-  }
-  private volatile AtomicBoolean isFinished = new AtomicBoolean();
-  public static void main(String[] args){
-    new Zork();
+    start();
+    
   }
   
-  public void connect(){
-    try {
-        final Socket socket = new Socket("127.0.0.1", 3000);
-      final Thread outThread = new Thread() {
-			@Override
-			public void run() {
-				System.out.println("Started...");
-				PrintWriter out = null;
-				Scanner sysIn = new Scanner(System.in);
-				try {
-					out = new PrintWriter(socket.getOutputStream());
-					out.println("Test");
-					out.flush();
-
-					while (sysIn.hasNext() && !isFinished.get()) {
-						String line = sysIn.nextLine();
-						if ("exit".equals(line)) {
-							synchronized (isFinished) {
-								isFinished.set(true);
-							}
-						}
-						out.println(line);
-						out.flush();
-						socket.close();
-					}
-				} catch (IOException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				} finally {
-					if (out != null) {
-						out.close();
-					}
-				}
-			};
-		};
-		outThread.start();
-
-		final Thread inThread = new Thread() {
-			@Override
-			public void run() {
-				// Use a Scanner to read from the remote server
-
-				Scanner in = null;
-				try {
-					in = new Scanner(socket.getInputStream());
-					String line = in.nextLine();
-					while (!isFinished.get()) {
-						System.out.println(line);
-						line = in.nextLine();
-					}
-				} catch (Exception e) {
-//					e.printStackTrace();
-				} finally {
-					if (in != null) {
-						in.close();
-					}
-				}
-			};
-		};
-		inThread.start();
-    } catch (UnknownHostException e) {
-      System.out.println("Unknown Host!");
-    } catch (IOException e) {
-      e.printStackTrace();
+  public void start(){
+    
+    System.out.println(Lang.intro);
+    running = true;
+    Scanner userInput = new Scanner(System.in);
+    while(running){
+      if(userInput.hasNext()){
+      	System.out.println("You said: " + userInput.next());
+      }
     }
+  }
+  
+  public static void main(String[] args){
+    
+    new Zork();
+    
   }
 }

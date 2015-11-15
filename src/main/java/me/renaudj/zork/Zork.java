c26123591121ca33eaddeec328c72e8d6e8802fb
Jordan Renaud <renaudj@windsor.k12.mo.us>
2015-11-14 16:41:03 -0800

    Simplified room creation and command registration processes.

--- src/main/java/me/renaudj/zork/Zork.java
+++ src/main/java/me/renaudj/zork/Zork.java
@@ -2,67 +2,79 @@ package me.renaudj.zork;
 
 import java.util.Scanner;
 
-public class Zork{
-  
-  public boolean running = false;
-  public CommandHandler commandHandler;
-  public Container inventory;
-  public Room currentRoom;
+public class Zork {
 
-    public Room room1 = new Room("Room1", Lang.room1);
-    public Room room2 = new Room("Room2", Lang.room2);
-  
-  public Zork(){
-	  commandHandler = new CommandHandler(this);
-	  commandHandler.register("quit", new Command(){
+    public boolean running = false;
+    public CommandHandler commandHandler;
+    public Container inventory;
+    public Room currentRoom;
 
-		public boolean onCommand(String command, String[] args) {
-			running = false;
-			System.out.println("Goodbye!");
-			return true;
-		}
-		  
-	  });
-      commandHandler.register("go", new Command(){
+    public final Room room1 = new Room("Room1", Lang.room1);
+    public final Room room2 = new Room("Room2", Lang.room2);
 
-          public boolean onCommand(String command, String[] args) {
-              if(args.length == 1){
-                  Direction d = Direction.valueOf(args[0].toUpperCase());
-                  goToRoom(currentRoom.getExit(d));
-                  return true;
-              } else {
-                  System.out.println("I don't understand.. do you mean \"go north\"?");
-              }
-              return false;
-          }
-      });
-      room1.addExit(Direction.UP, room2);
-      room2.addExit(Direction.DOWN, room1);
-    start();
-    
-  }
+    public Zork() {
+        commandHandler = new CommandHandler(this);
+        registerCommands();
+        setRoomExits();
+        addItemsToRooms();
+        start();
+    }
 
-    public void goToRoom(Room room){
-       currentRoom = room;
+    public void goToRoom(Room room) {
+        currentRoom = room;
         System.out.println(room.getDescription());
     }
-  
-  public void start(){
-    
-    System.out.println(Lang.intro);
-    running = true;
-    Scanner userInput = new Scanner(System.in);
-      goToRoom(room1);
-    while(running){
-      if(userInput.hasNextLine()){
-    	  commandHandler.handle(userInput.nextLine());
-      }
+
+    public void start() {
+
+        System.out.println(Lang.intro);
+        running = true;
+        Scanner userInput = new Scanner(System.in);
+        goToRoom(room1);
+        while (running) {
+            if (userInput.hasNextLine()) {
+                commandHandler.handle(userInput.nextLine());
+            }
+        }
     }
-  }
-  
-  public static void main(String[] args){
-    
-    new Zork();
-    
-  }
+
+    public void registerCommands() {
+        commandHandler.register("quit", new Command() {
+
+            public boolean onCommand(String command, String[] args) {
+                running = false;
+                System.out.println("Goodbye!");
+                return true;
+            }
+
+        });
+        commandHandler.register("go", new Command() {
+
+            public boolean onCommand(String command, String[] args) {
+                if (args.length == 1) {
+                    Direction d = Direction.valueOf(args[0].toUpperCase());
+                    goToRoom(currentRoom.getExit(d));
+                    return true;
+                } else {
+                    System.out.println("I don't understand.. do you mean \"go north\"?");
+                }
+                return false;
+            }
+        });
+    }
+
+    public void setRoomExits() {
+        room1.addExit(Direction.UP, room2);
+        room2.addExit(Direction.DOWN, room1);
+    }
+
+    public void addItemsToRooms() {
+
+    }
+
+    public static void main(String[] args) {
+
+        new Zork();
+
+    }
 }

d2c7d583f99285ae702958b652a77db9a899cbdf
Jordan Renaud <renaudj@windsor.k12.mo.us>
2015-11-18 13:41:23 -0600

    Added comments

--- src/main/java/me/renaudj/zork/Zork.java
+++ src/main/java/me/renaudj/zork/Zork.java
@@ -15,9 +15,11 @@ import java.util.List;
 import java.util.Scanner;
 
 public class Zork {
-
+    //Create rooms here
     public final Room room1 = new Room("Room1", Lang.room1);
     public final Room room2 = new Room("Room2", Lang.room2);
+
+
     public boolean running = false;
     public CommandHandler commandHandler;
     private Player player;
@@ -281,20 +283,22 @@ public class Zork {
         });
     }
 
+
+    //Assign neighboring rooms to the exits in each direction desired
     public void setRoomExits() {
         room1.addExit(Direction.UP, room2);
         room2.addExit(Direction.DOWN, room1);
     }
 
     public void populateRooms() {
-        Weapon sword = new Weapon("Divining Sword", 1000, 20, "Sword of the gods.", 10000, 20);
-        List<Item> items = new ArrayList<Item>();
-        items.add(sword);
-        Container chest = new Container("Chest", items);
-        room2.addItem(chest);
+        Weapon sword = new Weapon("Divining Sword", 1000, 20, "Sword of the gods.", 10000, 20); //create item of type Weapon
+        List<Item> items = new ArrayList<Item>(); //create list of items
+        items.add(sword); //add the sword to the list
+        Container chest = new Container("Chest", items); //create a new chest, filled with the list of items
+        room2.addItem(chest); //add the chest to the list of items in room 2
 
-        Character retard = new Character("Retard", 4, "CRICKEM NIGFOPS!");
-        retard.setRightHand(new Item("Dildo", 0, 1, "Penetrate them all!"));
-        room1.addCharacter(retard);
+        Character retard = new Character("Retard", 4, "CRICKEM NIGFOPS!"); //Create a new character called retard with a max HP of 4, and the caption CRICKEM NIGFOPS
+        retard.setRightHand(new Item("Dildo", 0, 1, "Penetrate them all!")); //Create a new item called dildo with max durability of 0, a weight of 1 unit, and the description: Penetrate them all!
+        room1.addCharacter(retard);//add the character to room1
     }
 }

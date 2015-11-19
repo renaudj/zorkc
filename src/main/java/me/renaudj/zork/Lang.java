9dca9dce0da9e2c0714c614f40b1159696dfee15
williamsc2 <williamsc2@windsor.k12.mo.us>
2015-11-19 09:46:09 -0600

    Merge branch 'master' of https://github.com/renaudj/zorkc
    Conflicts:
    	src/main/java/me/renaudj/zork/Room.java
    	src/main/java/me/renaudj/zork/Zork.java

--- src/main/java/me/renaudj/zork/Lang.java
+++ src/main/java/me/renaudj/zork/Lang.java
@@ -4,8 +4,7 @@ public class Lang {
 
     public static String title = "The Legend of Stelgin Putin";
     public static String intro = "Welcome to " + title + "!";
-    public static String room1 = "Zone 1a. You see nothing but white everywhere you look. There's a hidden door somewhere.";
-    public static String room2 = "Zone 2a. You've found the hidden door! You find yourself walking past the tattered and";
-    public static String room3a = "Zone 3a. You've found yourself in a lush grass field with the sound of birds chirping to the east."
+    public static String room1 = "Room1. You see nothing but white everywhere you look. There's a hidden door somewhere.";
+    public static String room2 = "Room2. You've found the hidden door!"
 
 }

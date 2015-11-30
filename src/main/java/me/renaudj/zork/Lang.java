dc087775a7c3fe31af7663005a58b9f7d86773f5
williamsc2 <williamsc2@windsor.k12.mo.us>
2015-11-30 10:07:55 -0600

    Added even more descriptions (not done yet)

--- src/main/java/me/renaudj/zork/Lang.java
+++ src/main/java/me/renaudj/zork/Lang.java
@@ -8,6 +8,10 @@ public class Lang {
     public static String r1a = "Sector 1a.\nYou've found the hidden door! You step out into a desolate wasteland of tattered and heavily tarnished metal walls in a place where machines once came to die.\nThe ground was covered in a confetti of mechanical pieces and the tombs of steel piled high into the smog hovering above.";
     public static String r2a = "Sector 2a.\nAfter continuing further you find a massive fence compiled of scrap and broken machines affectively arranged to keep something from getting in or even out?\nYou manage your way through a small gap in between sheet metal and barbed wire nearly losing a limb.";
     public static String r1b = "Sector 1b.\nManeuvering your way over piles of junk nearly impossible to walk on you spot a seemingly unpassable great wall of clutter.\n As you make your way closer you find a wrecked car built into the wall perfectly to climb through to the other side.";
-    public static String r2b = "Sector 2b.\n";
-    public static String r3a = "Sector 3a.\n";
+    public static String r2b = "Sector 2b.\nYou see vast areas of lush green grass with friendly wildlife roaming the land and beautiful blue skies soaring above.";
+    public static String r3a = "Sector 3a.\nYou come to a vibrant grass field with streams passing through the landscape and sounds of the wind whispering through the empty pastures.";
+    public static String r3b = "Sector 3b.\nScuttling your way through dense bushes you suddenly come to the edge of a ginormous canyon blocking passage to the east while nearly falling to your death.\nAfter recovering from your near death experience you notice a bulky object covered by a thick layer of old rotted branches and foilage.";
+    public static String r1c = "Sector 1c.\nMoving your way into an opening you are suddenly ambushed by a pack of rabid cyber rabbits and are unable to find a way out.";
+    public static String r2c = "Sector 2c.\nWith a sense of peace you stumble uppon a vivid meadow filled with beautiful and exotic flowers of all kinds with the sounds of birds chirping and streams flowing.";
+    public static String r3c = "Sector 3c.\nAmbushed by a diglet pod in a field of massive boulders about ten feet tall each.";
 }
